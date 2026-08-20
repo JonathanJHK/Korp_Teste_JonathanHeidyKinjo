77efc72c6520f0424713e6e162b910b0acade0c3
JonathanJHK <jonathanheidyjhk@gmail.com>
2026-08-20 00:41:31 -0300

    ✨ feat: initialize Angular application with routing, styles, and testing setup
    - Added app routing configuration.
    - Created main application component with title signal.
    - Set up initial styles and global stylesheets.
    - Implemented basic unit tests for the app component.
    - Configured TypeScript settings for the application and tests.
    - Added index.html as the entry point for the application.
    - Integrated Tailwind CSS and PrimeNG for UI components.

---
+++ frontend/korp-web/src/app/app.config.ts
@@ -0,0 +1,36 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import Aura from '@primeuix/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    providePrimeNG({
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.app-dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'theme, base, primeng',
+          },
+        },
+      },
+    }),
+  ],
+};
